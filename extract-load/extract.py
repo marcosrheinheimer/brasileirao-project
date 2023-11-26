e9b7b194f5a7e064665b76df527c432012ff02ac
Marcos Rheinheimer <marcosvrheinheimer@gmail.com>
2023-11-26 18:11:10 -0300

    Initial build

---
+++ extract-load/extract.py
@@ -0,0 +1,37 @@
+from urllib import request
+from dotenv import load_dotenv, find_dotenv
+import os
+import requests
+import json
+
+from google-stg import *
+
+
+def request_api(url: str, header: dict):
+    try:
+        r = requests.get(
+            url,
+            headers=header,
+            params=param
+        )
+
+        return r.json()
+    except Exception as e:
+        return e
+    
+
+if __name__ == '__main__':
+    load_dotenv(find_dotenv())
+
+    header = {
+        'X-RapidAPI-Key': os.getenv('RAPID_API_KEY'),
+        'X-RapidAPI-Host': os.getenv('RAPID_API_HOST')
+    }
+
+    param = {
+        'country': 'Brazil'
+    }
+
+    leagues = request_api('https://api-football-v1.p.rapidapi.com/v3/leagues', header)
+    leagues
+
